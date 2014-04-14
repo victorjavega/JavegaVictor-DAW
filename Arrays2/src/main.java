de85647c11d541f64ebe3598ce0ca1ce0e2933a6
Victor <victorjavega@gmail.com>
2014-04-14 08:59:47 +0200

    Arrays

---
+++ Arrays2/src/main.java
@@ -0,0 +1,35 @@
+
+public class main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//se crea el array
+		String inventario[]=new String[10];
+		// recorremos el array
+		for(int a=1;a<inventario.length;a++){
+		//valor de cada uno
+		inventario[0]="ropa";
+		inventario[1]="agua";
+		inventario[2]="muchos mecheros, pero muchos";
+		inventario[3]="saco de dormir";
+		inventario[4]="comida";
+		inventario[5]="caña de pescar";
+		inventario[6]="balon de futbol";
+		inventario[7]="movil";
+		inventario[8]="chicles";
+		inventario[9]="ps4";
+		
+		// comprobamos el resultado imprimiendo el resultado
+		System.out.println("me llevaria a la isla..: "+inventario[a]);
+		}
+		
+		//System.out.println("me llevo ->"+inventario[6]);
+	}
+
+
+
+
+
+}
